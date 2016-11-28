b2ac0289dc63a712fc4129381e6bfc73efce0931
msabirinsys <msabir@insys.com>
2016-11-28 12:29:49 -0500

    person type added

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./person.component.css']
 })
 export class PersonComponent implements OnInit {
+  testVar: string = 'This is Test';
+  person: Person = {
+      id: 0,
+      businessId: 0,
+      addressId: 0,
+      firstName: 'Muhammad',
+      lastName: 'Sabir',
+      phone: '6319839075',
+      email: 'msabir@insys.com',
+      personType: PersonType.CANDIDATE 
+  };
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() { 
   }
 
+  ngOnInit() {
+    
+  }
+}
+
+export class Person {
+  id: number;
+  businessId: number;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  personType: string;
+  addressId: number;
+}
+
+export class PersonType {
+    static EMPLOYEE : string = "Employee";
+    static CANDIDATE : string = "Candidate";
+    static PIVOTAL : string = "Pivotal";
+    static VENDOR : string = "Vendor";
 }
