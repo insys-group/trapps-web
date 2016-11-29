1c16842b1c8b5602cba8c80c2c4871450fbc0196
msabirinsys <msabir@insys.com>
2016-11-29 09:38:33 -0500

    refactored. adding routing, service and other components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,37 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { PersonComponent } from './person/person.component';
+import { PersonComponent } from './components/persons/person/person.component';
+import { PersonListComponent } from './components/persons/person-list/person-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BusinessListComponent } from './components/businesses/business-list/business-list.component';
+import { BusinessComponent } from './components/businesses/business/business.component';
+
+import { PersonService } from './services/person.service';
+import { AddressComponent } from './components/addresses/address/address.component';
+import { AddressListComponent } from './components/addresses/address-list/address-list.component';
+import { RoleComponent } from './components/roles/role/role.component';
+import { RoleListComponent } from './components/roles/role-list/role-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonComponent
+    PersonComponent,
+    PersonListComponent,
+    BusinessListComponent,
+    BusinessComponent,
+    AddressComponent,
+    AddressListComponent,
+    RoleComponent,
+    RoleListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [PersonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/persons/person-list/person-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { PersonService } from '../../../services/person.service';
+import { Person } from '../model/person.models';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'app-person-list',
+  templateUrl: './person-list.component.html',
+  styleUrls: ['./person-list.component.css']
+})
+
+export class PersonListComponent implements OnInit {
+
+  errorMessage: string;
+  persons: Person[];
+
+  constructor(private personService: PersonService) { }
+
+  getPersons() {
+    this.personService.getPersons()
+    .subscribe(
+      persons => this.persons = persons,
+      error => this.errorMessage = error
+    );
+  }
+  ngOnInit() {
+    this.getPersons();
+  }
+}
