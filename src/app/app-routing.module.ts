6af02a0cd9b2dde8cf2234805c8789940976d909
msabirinsys <msabir@insys.com>
2016-11-30 12:14:29 -0500

    added bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,16 @@ import { PersonComponent } from './components/persons/person/person.component';
 import { PersonListComponent } from './components/persons/person-list/person-list.component';
 import { BusinessComponent } from './components/businesses/business/business.component';
 import { BusinessListComponent } from './components/businesses/business-list/business-list.component';
+import { RoleComponent } from './components/roles/role/role.component';
+import { RoleListComponent } from './components/roles/role-list/role-list.component';
 
 const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'persons'},
     { path: 'persons', component: PersonListComponent},
     { path: 'person/:id', component: PersonComponent},
     { path: 'businesses', component: BusinessListComponent},
-    { path: 'business/:id', component: BusinessComponent}
+    { path: 'business/:id', component: BusinessComponent},
+    { path: 'roles', component: RoleListComponent}
 ];
 
 @NgModule({
