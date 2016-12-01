72ac8e3c1023a67351dd04eb7cd5fa1887f8a099
Kshitiz 'Kris' Krishna <kkrishnainsys.com@Kshitizs-MacBook-Pro.local>
2016-12-01 06:17:37 -0700

    Adding code for businesses

---
+++ src/app/services/business.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/catch'
+
+import { Business } from '../components/businesses/model/business.models';
+@Injectable()
+export class BusinessService {
+    constructor(private http: Http) {}
+
+    getBusinesses() {
+        return this.http.get('/api/businesses.json')
+        .map((response: Response) => <Business[]>response.json().data)
+        .do(data => console.log(data))
+        .catch(this.handleError);
+    }
+
+    private handleError(error: Response) {
+        console.error(error);
+        return Observable.throw(error.json().error || 'Server error');
+    }
+}
