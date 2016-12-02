d7a393d7dffa6408a1f8dbed91013859581b66ca
vnalitkin <vnalitkin@insys.com>
2016-12-02 11:42:50 -0500

    [TRAP-74] Adresss from browser

--- src/app/components/addresses/address-list/address-list.component.ts
+++ src/app/components/addresses/address-list/address-list.component.ts
@@ -1,26 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { AddressService } from '../../../services/address.service';
 import { Address } from '../../../models/address.model';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-address-list',
   templateUrl: './address-list.component.html',
   styleUrls: ['./address-list.component.css']
 })
+
 export class AddressListComponent implements OnInit {
  errorMessage: string;
-  address: Address[];
+  addresses: Address[];
 
   constructor(private addressService: AddressService) { }
 
   getAddresses() {
     this.addressService.getAddresses()
     .subscribe(
-      address => this.address = address,
+      addresses => this.addresses = addresses,
       error => this.errorMessage = error
     );
   }
+
   ngOnInit() {
     this.getAddresses();
   }
+
+    createNewAddress() {
+      console.log('will call new component');
+    }
 }
