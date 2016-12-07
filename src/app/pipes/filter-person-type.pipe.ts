809282e83ef8031557c3d438686cba057fc4cc8d
Rohit <1990.rohitnarwal@gmail.com>
2016-12-07 09:42:05 -0500

    [TRAP-70] working on PIPE

--- src/app/pipes/filter-person-type.pipe.ts
+++ src/app/pipes/filter-person-type.pipe.ts
@@ -6,9 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPersonTypePipe implements PipeTransform {
 
     transform(value: any, [personType]): any {
-            if (value==null) {
-      return null;
-    }
         return value.filter((item) => item.personType === personType);
     }
 }
