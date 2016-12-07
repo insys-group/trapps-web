01adecc622a12a1db11f3ad8846f4b3d126d7d1c
Rohit <1990.rohitnarwal@gmail.com>
2016-12-07 11:57:11 -0500

    [TRAP-70] fixed pipe error

--- src/app/pipes/filter-person-type.pipe.ts
+++ src/app/pipes/filter-person-type.pipe.ts
@@ -1,11 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filterPersonType'
+  name: 'filterPersonType',
 })
 export class FilterPersonTypePipe implements PipeTransform {
 
-    transform(value: any, [personType]): any {
-        return value.filter((item) => item.personType === personType);
+    transform(value: any, personType): any {
+        if (!value) {
+            return [];
+        }
+        return value.filter(function(item){
+            return item.personType === personType;
+        });
     }
 }
