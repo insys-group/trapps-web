f88d7b4c6e60abb3d90913a05e032417df064b07
vnalitkin <vnalitkin@insys.com>
2016-12-09 14:08:52 -0500

    [TRAP-83] Integration Buisness with Address

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { ResourceModule } from 'ng2-resource-rest';
 
 import { AppComponent } from './app.component';
 import { PersonComponent } from './components/persons/person/person.component';
@@ -54,6 +55,7 @@ import { PersonSkillsComponent } from './components/persons/person-skills/person
     HttpModule,
     NgbModule.forRoot(),
     InMemoryWebApiModule.forRoot(TrappsInMemoryDbService),
+    ResourceModule.forRoot(),
     AppRoutingModule
   ],
   providers: [PersonService, BusinessService, AddressService, NotificationService],
