a975d2dff81e0dbd23eb046c7e9534098cdccd1e
Rohit <1990.rohitnarwal@gmail.com>
2016-12-14 13:57:05 -0500

    [TRAP-91] Added role service and model. Now connected to rest api

--- src/app/components/roles/role-list/role-list.component.ts
+++ src/app/components/roles/role-list/role-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RoleService } from '../../../services/role.service';
+import { Roles } from '../../../models/roles.model';
+import { Observable } from 'rxjs/Observable';
+import { NotificationService } from '../../../services/notification.service';
+import { IResource } from '../../../resources/crud.resource';
 
 @Component({
   selector: 'app-role-list',
@@ -6,10 +12,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./role-list.component.css']
 })
 export class RoleListComponent implements OnInit {
+  closeResult: string;
 
-  constructor() { }
+  errorMessage: string;
+
+  roles: IResource[];
+
+  constructor(private router: Router, private roleService: RoleService, private notificationService: NotificationService) { }
 
   ngOnInit() {
+    this.getRoleList();  
   }
+
+  getRoleList() {
+    console.log('Enter: RoleListComponent.ngOnInit()');
+    this.roleService.getAll().subscribe(
+      roles => this.roles = roles.content,
+      error => this.notificationService.error(error.json().error)
+    );
+ }
 
 }
---
+++ src/app/models/roles.model.ts
@@ -0,0 +1,6 @@
+import { IResource } from '../resources/crud.resource';
+
+export class Roles extends IResource {
+    id: number;
+    name: string;
+}
