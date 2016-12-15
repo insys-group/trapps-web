da581da502e843a47dbf6e907a2fbebba50595ef
vnalitkin <vnalitkin@insys.com>
2016-12-15 11:07:54 -0500

    [TRAP-83] Integration Address to Business

---
+++ src/app/components/addresses/address-list-inner/address-list-inner.component.ts
@@ -0,0 +1,55 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { AddressService } from '../../../services/address.service';
+import { Address } from '../../../models/address.model';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { IResource } from '../../../resources/crud.resource';
+import { Http, Headers, Response } from '@angular/http';
+import { Injector } from '@angular/core';
+import {ConstantService} from  '../../../services/constant.service';
+import { NotificationService } from '../../../services/notification.service';
+
+@Component({
+  selector: 'app-address-list-inner',
+  templateUrl: './address-list-inner.component.html',
+  styleUrls: ['./address-list-inner.component.css']
+})
+
+export class AddressListInnerComponent implements OnInit {
+  closeResult: string;
+
+  errorMessage: string;
+  addresses: IResource[];
+  private addressService: AddressService;
+
+  constructor(private router: Router, 
+    private http: Http,
+    private injector: Injector,
+    private constantService: ConstantService,
+    private notificationService: NotificationService) 
+    {
+     console.log('AddressListComponent constructor');
+     this.addressService = new AddressService(http , injector);
+     }
+
+  ngOnInit() {
+    this.addressService.getAll()
+      .subscribe(
+        data => {this.addresses = data.content;},
+        error => this.notificationService.error(error.json().error)
+      );
+    console.log('out 1 ' + this.addresses);
+  }
+
+  onSelect(address: Address) {
+    this.router.navigate(['/addresses', address.id]);
+  }
+
+  create() {
+    this.router.navigate(['/addresses', 0]);
+  }
+
+    createNewAddress() {
+    console.log('will call new component');
+  }
+}
