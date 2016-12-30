b3bb9bd73f30a5cfaac839edc900b49c90564c6f
ericper <ericper@hotmail.com>
2016-12-29 20:26:45 -0500

    State drop list now populates US States; tweaked error message during Business List startup

--- src/app/components/addresses/address-list-inner/address-list-inner.component.ts
+++ src/app/components/addresses/address-list-inner/address-list-inner.component.ts
@@ -1,6 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Address } from '../../../models/address.model';
+import { Link } from '../../../models/rest.model';
+import { RestService } from '../../../services/rest.service';
+import { NotificationService } from '../../../services/notification.service'
+import { State } from '../../../models/state.model';
+import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs/Observable';
 
+export const stateURL = environment.STATE_URL + '?page=0&size=51';
+//need to fix restService.getAll() to return all data and not just the first page
 @Component({
   selector: 'app-address-list-inner',
   templateUrl: './address-list-inner.component.html',
@@ -9,8 +17,7 @@ import { Address } from '../../../models/address.model';
 
 export class AddressListInnerComponent implements OnInit {
  private _addresses = new Array<Address>();
- private _states = ["NY", "NC", "PA"];
-    
+ private _states:Array<string>;
   set addresses(addresses: Address[]) {
     console.log(`Enter: AddressComponent.set ${addresses}`);
     this._addresses = addresses;
@@ -19,13 +26,17 @@ export class AddressListInnerComponent implements OnInit {
     return this._addresses;
   }
 
-  constructor( ) 
-    {
-     console.log('AddressListComponent constructor');
-     }
+  constructor( 
+    private restService: RestService,
+    private notificationService: NotificationService
+            ) 
+        {
+          console.log('AddressListComponent constructor');
+        }
 
   ngOnInit() {
     console.log('ngOnInit() = ' + this.addresses);
+     this.loadStates();
   }
 
   add() {
@@ -38,6 +49,40 @@ export class AddressListInnerComponent implements OnInit {
 
    reset() {
     this._addresses = new Array<Address>();
+  }
+
+  /* private loadStates(): Observable<Array<State>> {
+      console.log(`Loading states data`);
+      return this.restService.getAll<State>(environment.STATE_URL)
+        .do(
+        states => {
+          this._states =
+           states.map(state => {
+             let businessState = new State();
+             businessState.id = state.id;
+              businessState.stateCode = state.stateCode;
+              return businessState;
+          });
+      },
+      error => this.handleError('Loading list of states failed.')
+      );
+  } */
+
+private loadStates(): void {
+    console.log(`Loading states data`);
+    //this.restService.getAll<State>(environment.STATE_URL) see comment on stateURL above
+    this.restService.getAll<State>(stateURL)  
+      .subscribe(
+        states => {
+          this._states = states.map (state=>state.stateCode);
+        },
+        error => this.handleError
+      );
+}
+
+  private handleError(error: any): void {
+    console.error('An error occurred', error);
+    this.notificationService.error('An Error occured ' + error);
   }
 
 }
