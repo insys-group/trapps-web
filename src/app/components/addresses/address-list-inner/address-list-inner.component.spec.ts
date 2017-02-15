2239b9eced0062c063897f2a7301e56c53c3dc88
Kshitiz 'Kris' Krishna <kkrishnainsys.com@Kshitizs-MacBook-Pro.local>
2017-02-14 20:25:53 -0700

    remove crud resource

--- src/app/components/addresses/address-list-inner/address-list-inner.component.spec.ts
+++ src/app/components/addresses/address-list-inner/address-list-inner.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { AddressListComponent } from './address-list.component';
+import { AddressListComponent } from '../address-list/address-list.component';
 
 describe('AddressListComponent', () => {
   let component: AddressListComponent;
