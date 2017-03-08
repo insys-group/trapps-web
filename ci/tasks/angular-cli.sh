57f2d15ce1713ceca3766a28e0a940da935aba6c
Armando Reyna <armando.i.reyna@gmail.com>
2017-03-08 13:05:02 -0700

    pipeline

---
+++ ci/tasks/angular-cli.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd trapps-web-source
+ng build --target=development --environment=dev
--- ci/tasks/npm-install.sh
+++ ci/tasks/npm-install.sh
@@ -2,5 +2,3 @@
 
 cd trapps-web-source
 npm install
-npm install -g angular-cli
-ng build --target=development --environment=dev
