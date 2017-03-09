0c068552c422e9511665354f2490ac07ef02ef31
Armando Reyna <armando.i.reyna@gmail.com>
2017-03-09 12:20:34 -0700

    test

--- ci/tasks/npm-install.sh
+++ ci/tasks/npm-install.sh
@@ -4,9 +4,4 @@ cd trapps-web-source
 npm install
 npm install -g angular-cli
 ng build --target=development --environment=dev
-ls -ll
-mv dist ../../dist
-cd ..
-cd ..
-mkdir put
-mv dist put/dist
+mv dist ../dist
