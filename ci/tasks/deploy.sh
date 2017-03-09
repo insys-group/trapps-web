09fcd2eb3ccb62231a23e3830426f9ad142b20ca
Armando Reyna <armando.i.reyna@gmail.com>
2017-03-09 12:08:25 -0700

    test

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 cp trapps-web-source/index.html package-output/index.html
+cp trapps-web-source/index.html package-output/index2.html
+cp trapps-web-source/index.html package-output/index3.html
 cd package-output
 ls -ll
 cd ..
